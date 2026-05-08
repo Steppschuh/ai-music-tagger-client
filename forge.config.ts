1604a3749061a0c6398963d81a05f4fdbec52278
Steppschuh <register@steppschuh.net>
2026-05-08 18:31:32 +0200

    Set executable name

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    executableName: 'ai-music-tagger',
     extraResource: [
       'bin/cli.js', // compiled CLI script bundled into resources/
     ],
